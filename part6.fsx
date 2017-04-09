52e24532a5be8e684c8f72f9f0bd888062c40cf6
contactsamie <contactsamie@gmail.com>
2017-04-09 17:14:56 -0400

    mmoniods

--- part6.fsx
+++ part6.fsx
@@ -2,5 +2,6 @@ let three=1+2
 let anotherThree=(+) 1 2
 let myThree=2|>((+) 1)
 let newThree=(1,2) |> (fun(x,y)-> (+) x y)
-
-three,anotherThree, myThree, newThree
+let add1=(+) 1
+let brandNewThree=add1 2
+three,anotherThree, myThree, newThree,add1, brandNewThree
---
+++ part7.fsx
@@ -0,0 +1,19 @@
+//this is a moniod coz all elements are moniods
+//rule 1:closure 1+2=2+1
+//rule 2: associativity (1+2)+3=1+(2+3)
+//rule 3: identity element 0+1=1+0,""+"hi"="hi"+"" etc
+type otderLine={
+    qty:int;
+    amount:float
+    }
+
+let orderLines=[
+ {qty=2; amount=10.0};
+ {qty=1; amount=10.2};
+ {qty=22; amount=10.7}
+]
+
+
+let addPair x  y={qty=(x.qty+y.qty);amount= (x.amount+y.amount)}
+
+orderLines |> Seq.reduce addPair
